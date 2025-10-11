6d537cfc6b801d5c919f7922cc233c3116513d0a
landsreyk <mr.andrey.kozyrev@gmail.com>
2025-10-11 12:28:01 +0300

    test fix

--- src/test/java/org/landsreyk/taskvault/security/TokenServiceTest.java
+++ src/test/java/org/landsreyk/taskvault/security/TokenServiceTest.java
@@ -3,8 +3,9 @@ package org.landsreyk.taskvault.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TokenServiceTest {
@@ -17,8 +18,23 @@ class TokenServiceTest {
 
     @Test
     void generateToken() {
-        UserDetails user = userDetailsService.loadUserByUsername("user");
-        String token = tokenService.generateAccessToken(user, null);
-        System.out.println(token);
+        var user = userDetailsService.loadUserByUsername("user");
+        var token = tokenService.generateAccessToken(user, null);
+        assertNotNull(token);
+    }
+
+    @Test
+    void extractUsernameFromToken() {
+        var user = userDetailsService.loadUserByUsername("user");
+        var token = tokenService.generateAccessToken(user, null);
+        var username = tokenService.extractUsername(token);
+        assertEquals("user", username);
+    }
+
+    @Test
+    void checkIfTokenIsValid() {
+        var user = userDetailsService.loadUserByUsername("user");
+        var token = tokenService.generateAccessToken(user, null);
+        assertTrue(tokenService.isTokenValid(token, user));
     }
 }
